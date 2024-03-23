fb0d8a926459001b8de962caa554716461b62da3
Daylion786 <usmanbaig2008@gmail.com>
2024-03-24 01:50:37 +0500

    nuber guessing game

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let comp_guess_Number = Math.floor(Math.random() * 6 + 1);
+let user_Guess_Number = await inquirer.prompt([
+    { message: "Enter your guess number:", type: "number", name: "guessnumber" }
+]);
+if (user_Guess_Number.guessnumber == comp_guess_Number) {
+    console.log("CongratulationS! you won a super bike!!");
+}
+else {
+    console.log("better luck be better next time :(");
+}
+;
